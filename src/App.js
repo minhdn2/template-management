9024c2575874c89c11132e29287aed6db8cf578a
Minh Duong <minhduongnhat@outlook.com>
2019-04-23 21:27:01 +0700

    template management

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Template from './Template';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  state = {
+    templates: [
+      { id: 1, imgSrc: 'templates/landerz/cover.png', title: 'Landerz', shortDescription: 'Landerz is your go-to solution when in search of the best free business landing page website template. Rock the online world with a contemporary, clean and light web design that will spark their curiosity.', previewLink: 'templates/landerz/landerz/index.html' },
+      { id: 2, imgSrc: 'templates/confer/cover.png', title: 'Confer', shortDescription: 'With a free HTML conference website template like Confer, you can market your event or any upcoming gathering like a champ. Kick it off in style and make a difference.', previewLink: 'templates/confer/index.html' },
+      { id: 3, imgSrc: 'templates/traveland/cover.png', title: 'Traveland', shortDescription: 'Indeed, Traveland is a free travel booking website template for travel agencies and other tourism businesses. Hammer out the best and most impressive travel page that will stand out a mile.', previewLink: 'templates/traveland/traveland/index.html' },
+      { id: 4, imgSrc: 'templates/quitelight/cover.png', title: 'Quitelight', shortDescription: 'Creativity knows no boundaries and nor does Quitelight free creative magazine website template. Get things going now and shine online with your fresh page.', previewLink: 'templates/quitelight/index.html' }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Templates Management!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome ^.^
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <Template templates={this.state.templates} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Template.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Template extends Component {
+    render() {
+        const { templates } = this.props;
+        const templateList = templates.map(template => {
+            return (
+                <div className="col-md-4" key={template.id}>
+                    <div className="card">
+                        <img className="card-img-top" src={template.imgSrc} alt="Card cover cap"></img>
+                        <div className="card-body">
+                            <h5 className="card-title">{template.title}</h5>
+                            <p className="card-text">{template.shortDescription}</p>
+                            <a href={template.previewLink} className="btn btn-primary">Preview</a>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <div className="row">
+                {templateList}
+            </div>
+        )
+    }
+}
+
+export default Template
